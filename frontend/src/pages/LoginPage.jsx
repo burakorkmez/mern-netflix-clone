43062e226c24bfa64c58b3cdbf2e6eb92cf0056c
Burak Orkmez <burakorkmezz@gmail.com>
2024-07-18 23:37:21 +0300

    loading states added

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,7 +6,7 @@ const LoginPage = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
-	const { login } = useAuthStore();
+	const { login, isLoggingIn } = useAuthStore();
 
 	const handleLogin = (e) => {
 		e.preventDefault();
@@ -58,8 +58,9 @@ const LoginPage = () => {
 							className='w-full py-2 bg-red-600 text-white font-semibold rounded-md
 							hover:bg-red-700
 						'
+							disabled={isLoggingIn}
 						>
-							Login
+							{isLoggingIn ? "Loading..." : "Login"}
 						</button>
 					</form>
 					<div className='text-center text-gray-400'>
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -10,7 +10,7 @@ const SignUpPage = () => {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 
-	const { signup } = useAuthStore();
+	const { signup, isSigningUp } = useAuthStore();
 
 	const handleSignUp = (e) => {
 		e.preventDefault();
@@ -76,8 +76,9 @@ const SignUpPage = () => {
 							className='w-full py-2 bg-red-600 text-white font-semibold rounded-md
 							hover:bg-red-700
 						'
+							disabled={isSigningUp}
 						>
-							Sign Up
+							{isSigningUp ? "Loading..." : "Sign Up"}
 						</button>
 					</form>
 					<div className='text-center text-gray-400'>
